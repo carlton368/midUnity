9e3cf757120ff746988c9e71e9b1ea21de427d08
이선아 <rkdql008@gmail.com>
2025-05-15 04:35:35 +0900

    thurjf
    htfsr

--- Assets/02_Scripts/UI Controll/ShootingUIManager.cs
+++ Assets/02_Scripts/UI Controll/ShootingUIManager.cs
@@ -1,13 +1,25 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class ShootingUIManager : MonoBehaviour
 {
     //싱글톤 전역 변수
     public static ShootingUIManager Instance;
+    [SerializeField] private GameObject killPanel;
     [SerializeField] private GameObject codePanel;
     [SerializeField] private GameObject winPanel;
     [SerializeField] private GameObject losePanel;
+    
+    // 각 패널의 CanvasGroup
+    private CanvasGroup killPanelCanvasGroup;
+    private CanvasGroup codePanelCanvasGroup;
+    private CanvasGroup winPanelCanvasGroup;
+    private CanvasGroup losePanelCanvasGroup;
+    
+    // 페이드 효과 속도 조절 변수
+    [SerializeField] private float fadeSpeed = 2.0f;
+    
     public WinorLose WinorLoseState;
     public enum WinorLose
     {
@@ -16,42 +28,114 @@ public class ShootingUIManager : MonoBehaviour
         lose = 2,
         code = 3,
     }
+    
     private void Awake()
     {
         Instance = this;
         DontDestroyOnLoad(gameObject);
         WinorLoseState = WinorLose.notsetted;
+        
+        // CanvasGroup 컴포넌트 가져오기 (없으면 추가)
+        killPanelCanvasGroup = GetOrAddCanvasGroup(killPanel);
+        codePanelCanvasGroup = GetOrAddCanvasGroup(codePanel);
+        winPanelCanvasGroup = GetOrAddCanvasGroup(winPanel);
+        losePanelCanvasGroup = GetOrAddCanvasGroup(losePanel);
+        
+        // 처음에는 모든 패널 비활성화 및 알파값 0으로 설정
+        SetPanelInitialState(killPanel, killPanelCanvasGroup);
+        SetPanelInitialState(codePanel, codePanelCanvasGroup);
+        SetPanelInitialState(winPanel, winPanelCanvasGroup);
+        SetPanelInitialState(losePanel, losePanelCanvasGroup);
     }
+    
+    private CanvasGroup GetOrAddCanvasGroup(GameObject panel)
+    {
+        CanvasGroup canvasGroup = panel.GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = panel.AddComponent<CanvasGroup>();
+        }
+        return canvasGroup;
+    }
+    
+    private void SetPanelInitialState(GameObject panel, CanvasGroup canvasGroup)
+    {
+        panel.SetActive(false);
+        canvasGroup.alpha = 0f;
+    }
+    
     public void ShowWinPanel()
     {
         Debug.Log("Win");
-        winPanel.SetActive(true);
+        StartCoroutine(FadePanel(winPanel, winPanelCanvasGroup, true));
         //GameManager.Instance.AutoRestartGame();
     }
     
     public void ShowLosePanel()
     {
         Debug.Log("Lose");
-        losePanel.SetActive(true);
+        StartCoroutine(FadePanel(losePanel, losePanelCanvasGroup, true));
+        //GameManager.Instance.AutoRestartGame();
+    }
+    
+    public void ShowKillPanel()
+    {
+        Debug.Log("Kill");
+        StartCoroutine(FadePanel(killPanel, killPanelCanvasGroup, true));
+        //GameManager.Instance.AutoRestartGame();
+    }
+    
+    public void CloseKillPanel()
+    {
+        Debug.Log("Close Kill");
+        StartCoroutine(FadePanel(killPanel, killPanelCanvasGroup, false));
         //GameManager.Instance.AutoRestartGame();
     }
     
     public void ShowCodePanel()
     {
         Debug.Log("Code");
-        codePanel.SetActive(true);
+        StartCoroutine(FadePanel(codePanel, codePanelCanvasGroup, true));
         //GameManager.Instance.AutoRestartGame();
     }
     
     public void CloseCodePanel()
     {
-        Debug.Log("Code");
-        codePanel.SetActive(false);
-        DieOrNot();
+        Debug.Log("Close Code");
+        StartCoroutine(FadePanel(codePanel, codePanelCanvasGroup, false, DieOrNot));
         //GameManager.Instance.AutoRestartGame();
     }
+    
     public void DieOrNot()
     {
         Debug.Log("Checking win/lose condition");
+    }
+    
+    // 패널 페이드 인/아웃 코루틴
+    private IEnumerator FadePanel(GameObject panel, CanvasGroup canvasGroup, bool fadeIn, System.Action onComplete = null)
+    {
+        // 페이드 인일 경우 패널 활성화
+        if (fadeIn)
+        {
+            panel.SetActive(true);
+        }
+        
+        float targetAlpha = fadeIn ? 1.0f : 0.0f;
+        
+        // 현재 알파값에서 목표 알파값까지 부드럽게 변경
+        while (!Mathf.Approximately(canvasGroup.alpha, targetAlpha))
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, targetAlpha, fadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+        
+        // 페이드 아웃일 경우 패널 비활성화
+        if (!fadeIn)
+        {
+            panel.SetActive(false);
+        }
+        
+        // 완료 후 콜백 실행
+        onComplete?.Invoke();
     }
 }
--- Assets/HTTP/Scripts/Samples/Sample_GetLatestQuizRequest.cs
+++ Assets/HTTP/Scripts/Samples/Sample_GetLatestQuizRequest.cs
@@ -53,6 +53,8 @@ namespace HTTP
                     {
                         Debug.LogWarning("서버에서 받은 퀴즈 ID가 비어 있습니다.");
                     }
+                    
+                    //Invoke(nameof(MonsterDie), delayTime);
                 }
                 catch (System.Exception ex)
                 {
@@ -81,6 +83,98 @@ namespace HTTP
         private void DelayedCall()
         {
             textUI.text = responseText;
+            //Invoke(nameof(PlayerDie), delayTime);
+            Invoke(nameof(MonsterDie), delayTime);
+        }
+        
+        private void PlayerDie()
+        {
+            ShootingUIManager.Instance.CloseCodePanel();
+            ShootingUIManager.Instance.ShowLosePanel();
+        }
+        private void MonsterDie()
+        {
+            ShootingUIManager.Instance.CloseCodePanel();
+            ShootingUIManager.Instance.ShowKillPanel();
+            Invoke("CallCloseKillPanel", delayTime);
+            
+            // 씬에 있는 모든 Monster 객체 찾기
+            GameObject[] monsters = GameObject.FindGameObjectsWithTag("Monster");
+            
+            // 각 몬스터에 대해 페이드 아웃 효과 적용
+            foreach (GameObject monster in monsters)
+            {
+                StartCoroutine(FadeOutMonster(monster, 2.0f));
+            }
+        }
+
+        // 몬스터 페이드 아웃 코루틴
+        private IEnumerator FadeOutMonster(GameObject monster, float duration)
+        {
+            if (monster == null) yield break;
+            
+            // 몬스터의 모든 렌더러 컴포넌트 가져오기
+            Renderer[] renderers = monster.GetComponentsInChildren<Renderer>();
+            
+            // 애니메이터 비활성화 (애니메이션 중지)
+            Animator animator = monster.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.enabled = false;
+            }
+            
+            // 몬스터 AI 비활성화 (움직임 중지)
+            MonsterAI_NoNavMesh monsterAI = monster.GetComponent<MonsterAI_NoNavMesh>();
+            if (monsterAI != null)
+            {
+                monsterAI.enabled = false;
+            }
+            
+            float elapsedTime = 0f;
+            
+            // 기존 머티리얼과 색상 저장
+            Material[][] originalMaterials = new Material[renderers.Length][];
+            Color[][] originalColors = new Color[renderers.Length][];
+            
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                originalMaterials[i] = renderers[i].materials;
+                originalColors[i] = new Color[originalMaterials[i].Length];
+                
+                for (int j = 0; j < originalMaterials[i].Length; j++)
+                {
+                    // 원래 색상 저장
+                    originalColors[i][j] = originalMaterials[i][j].color;
+                }
+            }
+            
+            // 시간에 따라 알파값 감소
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                float alpha = Mathf.Clamp01(1 - (elapsedTime / duration));
+                
+                // 모든 렌더러의 모든 머티리얼 투명도 조정
+                for (int i = 0; i < renderers.Length; i++)
+                {
+                    for (int j = 0; j < originalMaterials[i].Length; j++)
+                    {
+                        Color newColor = originalColors[i][j];
+                        newColor.a = alpha;
+                        originalMaterials[i][j].color = newColor;
+                    }
+                }
+                
+                yield return null;
+            }
+            
+            // 페이드 아웃 완료 후 몬스터 제거
+            Destroy(monster);
+        }
+
+        private void CallCloseKillPanel()
+        {
+            ShootingUIManager.Instance.CloseKillPanel();
         }
     }
 }
